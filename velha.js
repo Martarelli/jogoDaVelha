bcad99f83b4b59e4776c6e4235a101837690a67f
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-01 18:35:23 -0300

    verificação se valor já foi alterado

--- velha.js
+++ velha.js
@@ -6,11 +6,19 @@ mudarJogador('X');
 function escolherQuadrado(id) {
     
     var quadrado = document.getElementById(id);
-
+    if (quadrado.innerHTML !== '-') {
+        return;
+    }
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#000';
 
+    if (jogador === 'X'){
+        jogador = "O"; 
+    } else {
+        jogador = 'X';
+    }
 
+    mudarJogador(jogador)
 }
 
 function mudarJogador (valor){
